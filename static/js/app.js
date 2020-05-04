9aa6769cba3df03b253ff876d39b4c856092b26b
Kalkidanalemaye <59233048+Kalkidanalemaye@users.noreply.github.com>
2020-05-04 16:26:35 -0400

    filtered data

--- static/js/app.js
+++ static/js/app.js
@@ -24,3 +24,32 @@ data.forEach((dataRow) => {
 }
 
 
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    
+// pseudocode practice
+//if (a date is entered) {
+	//Filter the default data to show only the date entered
+//};
+
+if (date) {
+    // Apply `filter` to the table data to only keep the
+    // rows where the `datetime` value matches the filter value
+	filteredData = filteredData.filter(row => row.datetime === date);
+};
+  
+// Rebuild the table using the filtered data
+// @NOTE: If no date was entered, then filteredData will
+// just be the original tableData.
+buildTable(filteredData);
+
+// Attach an event to listen for the form button
+d3.select("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
+
+}
+
+
